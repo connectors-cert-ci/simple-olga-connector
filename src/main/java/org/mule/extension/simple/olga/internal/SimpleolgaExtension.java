a3505ce9818d0b57c26476192afee13697a7614a
connectors-cert-ci <connectors-cert-ci@mulesoft.com>
2024-09-30 15:57:40 +0530

    Update SimpleolgaExtension.java

--- src/main/java/org/mule/extension/simple/olga/internal/SimpleolgaExtension.java
+++ src/main/java/org/mule/extension/simple/olga/internal/SimpleolgaExtension.java
@@ -5,6 +5,8 @@ import org.mule.runtime.extension.api.annotation.Extension;
 import org.mule.runtime.extension.api.annotation.Configurations;
 import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
 import org.mule.runtime.extension.api.annotation.license.RequiresEntitlement;
+import org.mule.sdk.api.annotation.JavaVersionSupport;
+import org.mule.sdk.api.meta.JavaVersion;
 
 
 /**
@@ -15,6 +17,7 @@ import org.mule.runtime.extension.api.annotation.license.RequiresEntitlement;
 @Extension(name = "Simple-olga-", category = Category.CERTIFIED, vendor = "Olga")
 @Configurations(SimpleolgaConfiguration.class)
 @RequiresEntitlement(name="Simple-olga-")
+@JavaVersionSupport({JavaVersion.JAVA_8, JavaVersion.JAVA_11, JavaVersion.JAVA_17})
 public class SimpleolgaExtension {
 
 }
